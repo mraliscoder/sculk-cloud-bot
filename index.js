0f812a07f5f73c55e09ff4b787434d1d3f13a0e3
Eduard Ilin <github@dpkgsoft.com>
2024-02-13 14:07:15 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,20 +1,4 @@
 require("dotenv").config();
-
-console.log("");
-console.log(" Welcome to default XorekCloud bot! ");
-console.log("");
-console.log(" You can test our hosting by running this bot,");
-console.log(" or you can upload your own bot.");
-console.log("");
-console.log(" To run this bot create file '.env' in the root");
-console.log(" folder with TOKEN= parameter. After '=' place bot token");
-console.log(" from https://discord.com/developers/applications.");
-console.log(" This bot is also available at https://qdk.pw/xrkds.");
-console.log("");
-console.log(" This bot will respond on command 'ping'.");
-console.log(" Have fun!");
-console.log("");
-
 if (process.env.TOKEN === undefined) {
     process.exit(100);
 }
@@ -25,13 +9,10 @@ const client = new Client({
 });
 
 client.on("ready", () => {
-  console.log("I am ready!");
-});
-
-client.on("messageCreate", (message) => {
-  if (message.content.startsWith("ping")) {
-    message.channel.send("pong!");
-  }
+    client.user.setPresence({
+      activities: [{ name: `sculk.cloud`, type: ActivityType.Watching }],
+      status: 'dnd',
+    });
 });
 
 client.login(process.env.TOKEN);
